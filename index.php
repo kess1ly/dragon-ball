c82202ac0258ce6854ac3208d9816e8af0aacf17
kess1ly <kekezinha2905@gmail.com>
2026-09-15 20:51:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Dragon Ball Archive</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <main 
+    class="tela-inicial"
+    style="background-image: url('fundo-dragon-ball.png.png');"
+>
+
+        <div class="conteudo">
+
+            <h1>DRAGON BALL ARCHIVE</h1>
+
+            <h2>FAÇA SUA VIAGEM</h2>
+
+            <p>
+                Explore o universo Dragon Ball,
+                descubra personagens, técnicas e muito mais.
+            </p>
+
+            <a href="cadastro_usuario.php" class="botao">
+                🚀 FAÇA SUA VIAGEM
+            </a>
+
+        </div>
+
+    </main>
+
+</body>
+
+</html>
